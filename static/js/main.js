3b2f70ef633c131374da42d9cdc72a0bc5e0d29b
Zarino Zappia <mail@zarino.co.uk>
2025-03-05 15:45:51 +0000

    Reuse same autocomplete for header and homepage search
    This both enables searching the homepage “Scorecard” table by postcode,
    and handles the case of multiple matches much more clearly (since
    they’re now displayed in the autocomplete dropdown, rather than just
    highlighted in the table).
    
    Thanks to Lee from the Woodland Trust for the suggestion!

--- static/js/main.js
+++ static/js/main.js
@@ -3,92 +3,100 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
 });
 
+function setUpGeocodeAutocomplete(inputElement, singleResultCallback) {
+    var awesomplete = new Awesomplete(
+        inputElement,
+        {
+            list: _.pluck(window.councils, 'council_name'),
+            minChars: 3,
+            autoFirst: true
+        }
+    );
+
+    awesomplete.container.addEventListener('awesomplete-select', function(selection){
+        selection.preventDefault();
+        var council = _.findWhere(
+            window.councils,
+            {'council_name': selection.text.value}
+        );
+        singleResultCallback(council);
+    });
+
+    $(inputElement).on('keypress', function (e) {
+        // Reset Awesomplete overrides from last geocoding result.
+        awesomplete.list = _.pluck(window.councils, 'council_name');
+        awesomplete.filter = Awesomplete.FILTER_CONTAINS;
+
+        if (e.which === 13) {
+            e.preventDefault();
+
+            // If the user presses Enter, we assume they’ve entered a
+            // postcode and intend to search by it. We tidy the postcode
+            // then send it to MapIt. (If the text isn’t a valid postcode,
+            // MapIt will return an error, which we just silently ignore.)
+            var postcode = inputElement.value.replaceAll(' ', '').toLowerCase();
+            $.ajax({
+                url: 'https://mapit.mysociety.org/postcode/' + encodeURIComponent(postcode) + '.json',
+                dataType: 'json'
+            }).done(function(data){
+                var areas = _.values(data.areas);
+                var matches = _.filter(areas, function(area){
+                    return [
+                        'CTY', // County councils (two-tier)
+                        'COI', // Isles of Scilly is a Unitary authority (single-tier)
+                        'DIS', // Non-Metropolitan Districts (two-tier)
+                        'GLA', // Greater London Authority (which we treat as a Combined Authority)
+                        'LBO', // London Boroughs (single-tier)
+                        'LGD', // Nothern Irish councils (single-tier)
+                        'MTD', // Metropolitan Districts (single-tier)
+                        'UTA', // Unitary Authorities (single-tier)
+                    ].indexOf(area.type) > -1;
+                });
+
+                // If MapIt returns a single area, redirect to it
+                // (as if it had been selected from the Awesomplete).
+                if (matches.length == 1) {
+                    var council = _.findWhere(
+                        window.councils,
+                        {'council_gss': matches[0].codes.gss}
+                    );
+                    singleResultCallback(council);
+
+                // If MapIt returns multiple areas (in the case of
+                // two-tier authorities, eg: CB3 9DR) override the
+                // Awesomplete to display them in a dropdown menu.
+                } else if (matches.length > 1) {
+                    // Get matching councils from window.councils (matching by GSS code).
+                    var councils = _.filter(window.councils, function(council){
+                        return _.map(matches, function(match){
+                            return match.codes.gss;
+                        }).indexOf(council.council_gss) > -1;
+                    });
+
+                    // Note: We will revert these Awesomplete overrides
+                    // on the next keypress.
+                    awesomplete.list = _.pluck(councils, 'council_name');
+                    awesomplete.filter = function(){ return true; }
+                    awesomplete.evaluate();
+                }
+            });
+        }
+    });
+}
+
 $(function(){
     $('.scorecard__data__table, .mini-scorecard').on('click', 'tbody tr', function(){
         window.location = $(this).find('a').eq(0).attr('href');
     });
 
     $('input[name="pc"]').each(function(){
-        var $pc = $(this);
-
-        var awesomplete = new Awesomplete(
-            $pc[0],
-            {
-                list: _.pluck(window.councils, 'council_name'),
-                minChars: 3,
-                autoFirst: true
+        var inputElement = this;
+        setUpGeocodeAutocomplete(
+            inputElement,
+            function (council) {
+                window.location = window.baseurl + '/councils/' + council.council_slug;
             }
         );
-
-        document.addEventListener('awesomplete-select', function(selection){
-            selection.preventDefault();
-            var council = _.findWhere(
-                window.councils,
-                {'council_name': selection.text.value}
-            );
-            window.location = window.baseurl + '/councils/' + council.council_slug + '#councils';
-        });
-
-        $pc.on('keypress', function (e) {
-            // Reset Awesomplete overrides from last geocoding result.
-            awesomplete.list = _.pluck(window.councils, 'council_name');
-            awesomplete.filter = Awesomplete.FILTER_CONTAINS;
-
-            if (e.which === 13) {
-                e.preventDefault();
-
-                // If the user presses Enter, we assume they’ve entered a
-                // postcode and intend to search by it. We tidy the postcode
-                // then send it to MapIt. (If the text isn’t a valid postcode,
-                // MapIt will return an error, which we just silently ignore.)
-                var postcode = $pc.val().replaceAll(' ', '').toLowerCase();
-                $.ajax({
-                    url: 'https://mapit.mysociety.org/postcode/' + encodeURIComponent(postcode) + '.json',
-                    dataType: 'json'
-                }).done(function(data){
-                    var areas = _.values(data.areas);
-                    var matches = _.filter(areas, function(area){
-                        return [
-                            'CTY', // County councils (two-tier)
-                            'COI', // Isles of Scilly is a Unitary authority (single-tier)
-                            'DIS', // Non-Metropolitan Districts (two-tier)
-                            'GLA', // Greater London Authority (which we treat as a Combined Authority)
-                            'LBO', // London Boroughs (single-tier)
-                            'LGD', // Nothern Irish councils (single-tier)
-                            'MTD', // Metropolitan Districts (single-tier)
-                            'UTA', // Unitary Authorities (single-tier)
-                        ].indexOf(area.type) > -1;
-                    });
-
-                    // If MapIt returns a single area, redirect to it
-                    // (as if it had been selected from the Awesomplete).
-                    if (matches.length == 1) {
-                        var council = _.findWhere(
-                            window.councils,
-                            {'council_gss': matches[0].codes.gss}
-                        );
-                        window.location = window.baseurl + '/councils/' + council.council_slug + '#councils';
-
-                    // If MapIt returns multiple areas (in the case of
-                    // two-tier authorities, eg: CB3 9DR) override the
-                    // Awesomplete to display them in a dropdown menu.
-                    } else if (matches.length > 1) {
-                        // Get matching councils from window.councils (matching by GSS code).
-                        var councils = _.filter(window.councils, function(council){
-                            return _.map(matches, function(match){
-                                return match.codes.gss;
-                            }).indexOf(council.council_gss) > -1;
-                        });
-
-                        // Note: We will revert these Awesomplete overrides
-                        // on the next keypress.
-                        awesomplete.list = _.pluck(councils, 'council_name');
-                        awesomplete.filter = function(){ return true; }
-                        awesomplete.evaluate();
-                    }
-                });
-            }
-        });
     });
 
     $('#scorecard-filters-detail').on('change', function(e){
@@ -139,26 +147,19 @@ $(function(){
         });
     });
 
-    $('#scorecard-table-search').on('keyup', function(){
-        var searchTerm = $.trim($(this).val());
-        var $table = $('#scorecard-table');
-
-        $table.find('tr.highlight').removeClass('highlight');
-
-        if(searchTerm.length > 1) {
-            var matches = _.filter(window.councils, function(council){
-                return council.council_name.toUpperCase().indexOf(searchTerm.toUpperCase()) > -1;
-            });
-            _.each(matches, function(match){
-                $table.find('tr[data-council-slug="' + match.council_slug + '"]').addClass('highlight');
-            });
-            if (matches.length === 1) {
-                $table.find('tr[data-council-slug="' + matches[0].council_slug + '"]').eq(0)[0].scrollIntoView({
+    $('#scorecard-table-search').each(function(){
+        var inputElement = this;
+        setUpGeocodeAutocomplete(
+            inputElement,
+            function (council) {
+                var $row = $('#scorecard-table').find('tr[data-council-slug="' + council.council_slug + '"]').eq(0);
+                $row.addClass('highlight').siblings('.highlight').removeClass('highlight');
+                $row[0].scrollIntoView({
                     block: 'center',
                     inline: 'center'
                 });
             }
-        }
+        );
     });
 
     $('[data-copy-content]').on('click', function(e){
